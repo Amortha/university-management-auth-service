a20fdbaca4032863607dc6860bc6b40e35a4ccd2
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-05-31 18:58:44 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import app from './app'
-import config from "./config/index"
+import config from './config/index'
 
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string)
     console.log(`Database is connected successfully`)
-   
+
     app.listen(config.port, () => {
-        console.log(`Application listening on port ${config.port}`)
-      })
-
-} catch (err) {
-
-        console.log('Failed top connect database',err)
-    }
-
-
-
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Failed top connect database', err)
+  }
 }
-bootstrap();
+bootstrap()
