727ab4bd46c45e1dc6fe457eb0aea843792ba67e
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-06-04 13:08:54 +0600

    logger

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,11 +1,21 @@
 /* eslint-disable no-undef */
 import { createLogger, format, transports } from 'winston'
 import path from 'path'
-// const { combine, timestamp, label, printf } = format;
+const { combine, timestamp, label, printf, prettyPrint } = format
+
+//Custom log Format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds}}[${label}] ${level}: ${message}`
+})
 
 const logger = createLogger({
   level: 'info',
-  format: format.json(),
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat, prettyPrint()),
   transports: [
     new transports.Console(),
     new transports.File({
@@ -16,7 +26,7 @@ const logger = createLogger({
 })
 const errorlogger = createLogger({
   level: 'error',
-  format: format.json(),
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
     new transports.File({
