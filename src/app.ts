ea7468a5003e1b2b6b3f0b1a25ff1835bbb066d5
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-06-11 13:13:00 +0600

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
-import express, { Application } from 'express';
+/* eslint-disable no-undef */
 import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routers from './app/routes';
+
 // import { UsersRouter } from './app/modules/user/user.route';
 // import { AcademicSemesterRouters } from './app/modules/academicSemester/academicSemester.route';
 // import { Promise } from 'mongoose'
@@ -27,5 +30,20 @@ app.use('/api/v1/', routers);
 // })
 //global error handler
 app.use(globalErrorHandler);
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
