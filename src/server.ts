5463edf18c6b72717820011925d86884f191c5a4
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-06-20 23:30:25 +0600

    vercel

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,9 @@ import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config/index';
-import { errorlogger, logger } from './shared/logger';
 
 process.on('uncaughtException', error => {
-  errorlogger.error(error);
+  console.log(error);
   process.exit(1);
 });
 
@@ -14,19 +13,19 @@ let server: Server;
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    logger.info(`Database is connected successfully`);
+    console.log(`Database is connected successfully`);
 
     server = app.listen(config.port, () => {
-      logger.info(`Application listening on port ${config.port}`);
+      console.log(`Application listening on port ${config.port}`);
     });
   } catch (err) {
-    errorlogger.error('Failed top connect database', err);
+    console.log('Failed top connect database', err);
   }
 
   process.on('unhandleRection', error => {
     if (server) {
       server.close(() => {
-        errorlogger.error(error);
+        console.log(error);
         process.exit(1);
       });
     } else {
@@ -37,7 +36,7 @@ async function bootstrap() {
 bootstrap();
 
 process.on('SIGALRM', () => {
-  logger.info('SIGALRM is received');
+  console.log('SIGALRM is received');
   if (server) {
     server.close();
   }
